479456d07f60556c595bb482d6bb4c11cc441d65
Adam DeLessio <adamdelessio@gmail.com>
2020-01-16 10:33:40 -0500

    user is prompted to add a new contact and the info is stored in the db

---
+++ app.py
@@ -0,0 +1,2 @@
+
+opening = input("Would you like to add a contact or view existing ones? Type 'add' or 'view': ")
--- main.py
+++ main.py
@@ -11,7 +11,22 @@ class Contact(BaseModel):
 	fname = CharField()
 	lname = CharField()
 	email = CharField()
-	phone = IntegerField()
+	# phone = IntegerField()
 
 db.connect()
+db.drop_tables([Contact])
 db.create_tables([Contact])
+
+# adam = Contact(fname='Adam', lname='DeLessio', email='adamdelessio@gmail.com')
+# adam.save()
+
+def on_load():
+	home = input("Would you like to add a contact or view existing ones? Type 'add' or 'view': ")
+	if home == 'add':
+		fname = input("Enter first name: ")
+		lname = input("Enter last name: ")
+		email = input("Enter email: ")
+	new_contact = Contact(fname=fname, lname=lname, email=email)
+	new_contact.save()
+
+on_load()
