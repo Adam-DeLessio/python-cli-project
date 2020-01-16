1995a6ba430089cadb2870a60322e7a6716fc780
Adam DeLessio <adamdelessio@gmail.com>
2020-01-16 11:20:02 -0500

    user can delete existing contacts

--- main.py
+++ main.py
@@ -17,31 +17,48 @@ db.connect()
 db.create_tables([Contact])
 
 def on_load():
-	home = input("Would you like to add a contact or view existing ones? Type 'a' or 'v': ")
-	if home == 'add':
+	home = input("Would you like to add a contact or view, update, or delete an existing one? Type 'a', 'v', 'u', or 'd': ")
+	if home == 'a':
 		fname = input("Enter first name: ")
 		lname = input("Enter last name: ")
 		email = input("Enter email: ")
+		print("Contact added!")
 		new_contact = Contact(fname=fname, lname=lname, email=email)
 		new_contact.save()
 		on_load()
-	elif home == 'view':
+	elif home == 'v':
 		view = input("View all or search? Type 'va' or 's': ")
 		if view == 'va':
-			contacts = Contact.select()
-			for c in contacts:
+			view_contacts = Contact.select()
+			for c in view_contacts:
 				print(c.fname, c.lname, c.email)
 			on_load()
-		elif view == 'search':
+		elif view == 's':
 			search_name = input("Search by first name: ")
-			contacts = Contact.select()
-			for c in contacts:
+			search_contacts = Contact.select()
+			for c in search_contacts:
 				if search_name == c.fname:
 					print(c.fname, c.lname, c.email)
-				else:
-					print("No contacts by that name")
 					on_load()
+				# else:
+				# 	print("No contacts by that name.")
+				# 	on_load()
+	
 
+
+	elif home == 'd':
+		seek_destroy = input("Search by first name for the contact you want to delete: ")
+		delete_contacts = Contact.select()
+		for c in delete_contacts:
+			if seek_destroy == c.fname:
+				affirm = input("Are you sure you want to utterly DESTROY this person? y/n: ")
+				if affirm == 'y':
+					goodbye = Contact.get(Contact.fname == seek_destroy)
+					goodbye.delete_instance()
+					on_load()
+				elif affirm == 'n':
+					print("Oh, ok. That was close.")
+					on_load()
 
 
 
