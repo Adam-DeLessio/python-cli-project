139a95ce1918b409ddd407fc2be83be32876d3a3
Adam DeLessio <adamdelessio@gmail.com>
2020-01-16 13:05:32 -0500

    user must type valid commands or contact names or else they will be redirected to the home screen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Contact(BaseModel):
 	fname = CharField(null = False)
 	lname = CharField(null = False)
 	email = CharField(null = False)
-	# phone = IntegerField()
+	phone = IntegerField(null = False)
 
 db.connect()
 # db.drop_tables([Contact])
@@ -20,36 +20,56 @@ def on_load():
 	home = input("Would you like to add a contact or view, update, or delete an existing one? Type 'a', 'v', 'u', or 'd': ")
 	if home == 'a':
 		fname = input("Enter first name: ")
+		if fname == 'x':
+			on_load()
 		lname = input("Enter last name: ")
+		if lname == 'x':
+			on_load()
 		email = input("Enter email: ")
+		if email == 'x':
+			on_load()
+		phone = input("Enter phone number: ")
+		# phone = str(phone)
+		if phone == 'x':
+			on_load()
+		# format_phone = []
+
+		# for char in phone:
+		# 	format_phone.append(char)
+		# new_phone = ''.join(str(f"({format_phone[0:3]}) {format_phone[3:6]} {format_phone[6:]}"))
+
 		print("Contact added!")
-		new_contact = Contact(fname=fname, lname=lname, email=email)
+		new_contact = Contact(fname=fname, lname=lname, email=email, phone=phone)
 		new_contact.save()
 		on_load()
 	elif home == 'v':
 		view = input("View all or search? Type 'va' or 's': ")
+		if view == 'x':
+			on_load()
 		if view == 'va':
 			view_contacts = Contact.select()
 			for c in view_contacts:
-				print(c.fname, c.lname, c.email)
+				print(c.fname, c.lname, c.email, c.phone)
 			on_load()
 		elif view == 's':
 			search_name = input("Search by first name: ")
 			search_contacts = Contact.select()
 			for c in search_contacts:
 				if search_name == c.fname:
-					print(c.fname, c.lname, c.email)
+					print(c.fname, c.lname, c.email, c.phone)
 					on_load()
-				# else:
-				# 	print("No contacts by that name.")
-				# 	on_load()
+				else:
+					print("Contact does not exist.")
+				on_load()
 	elif home == 'u':
 		update_contact = input("Search by first name for the contact you want to update: ")
+		if update_contact == 'x':
+			on_load()
 		search_update = Contact.select()
 		for c in search_update:
 			if update_contact == c.fname:
 				new_update = Contact.get(Contact.fname == update_contact)
-				update_field = input("Which field do you want to update? fname/lname/email: ")
+				update_field = input("Which field do you want to update? fname/lname/email/phone: ")
 				if update_field == 'fname':
 					new_update.fname = input("New first name: ")
 					new_update.save()
@@ -59,10 +79,17 @@ def on_load():
 				elif update_field == 'email':
 					new_update.email == input("New email: ")
 					new_update.save()
-				on_load()
+				elif update_field == 'phone':
+					new_update.phone == int(input("New phone number: "))
+					new_update.save()
+			else:
+				print("Contact does not exist.")
+			on_load()
 
 	elif home == 'd':
 		seek_destroy = input("Search by first name for the contact you want to delete: ")
+		if seek_destroy == 'x':
+			on_load()
 		delete_contacts = Contact.select()
 		for c in delete_contacts:
 			if seek_destroy == c.fname:
@@ -74,14 +101,13 @@ def on_load():
 				elif affirm == 'n':
 					print("Oh, ok. That was close.")
 					on_load()
+			else:
+				print("Contact does not exist.")
+			on_load()
 
 	else:
 		print("Please type one of the approved commands.")
 		on_load()
-
-
-
-
 
 
 on_load()
