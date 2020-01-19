8207a4a2af116dc888bd55ca911198e1a00aadc0
Adam DeLessio <adamdelessio@gmail.com>
2020-01-18 22:22:38 -0500

    added some better formatting

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ class Contact(BaseModel):
 	fname = CharField(null = False)
 	lname = CharField(null = False)
 	email = CharField(null = False)
-	phone = IntegerField(null = False)
+	phone = CharField(null = False)
 
 db.connect()
-# db.drop_tables([Contact])
-# db.create_tables([Contact])
+db.drop_tables([Contact])
+db.create_tables([Contact])
+
+new_contact = None
 
 def on_load():
 	home = input("Would you like to add a contact or view, update, or delete an existing one? Type 'a', 'v', 'u', or 'd': ")
@@ -29,17 +31,19 @@ def on_load():
 		if email == 'x':
 			on_load()
 		phone = input("Enter phone number: ")
-		# phone = str(phone)
 		if phone == 'x':
 			on_load()
-		# format_phone = []
 
+		# format_phone = []
 		# for char in phone:
 		# 	format_phone.append(char)
-		# new_phone = ''.join(str(f"({format_phone[0:3]}) {format_phone[3:6]} {format_phone[6:]}"))
+		# area = ''.join(str(format_phone[0:3]))
+		new_phone = '(' + phone[0:3] + ') ' + phone[3:6] + '-' + phone[6:]
+		# new_phone = f"({area}) {format_phone[3:]}"
+		# new_phone = ''.join(f"({format_phone[0:3]}) {format_phone[3:6]}-{format_phone[6:]}")
 
 		print("Contact added!")
-		new_contact = Contact(fname=fname, lname=lname, email=email, phone=phone)
+		new_contact = Contact(fname=fname, lname=lname, email=email, phone=new_phone)
 		new_contact.save()
 		on_load()
 	elif home == 'v':
@@ -49,18 +53,18 @@ def on_load():
 		if view == 'va':
 			view_contacts = Contact.select()
 			for c in view_contacts:
-				print(c.fname, c.lname, c.email, c.phone)
+				print(f"{c.fname} | {c.lname} | {c.email} | {c.phone}")
 			on_load()
 		elif view == 's':
 			search_name = input("Search by first name: ")
 			search_contacts = Contact.select()
 			for c in search_contacts:
 				if search_name == c.fname:
-					print(c.fname, c.lname, c.email, c.phone)
+					print(f"{c.fname} | {c.lname} | {c.email} | {c.phone}")
 					on_load()
-				else:
-					print("Contact does not exist.")
-				on_load()
+			else:
+				print("Contact does not exist.")
+			on_load()
 	elif home == 'u':
 		update_contact = input("Search by first name for the contact you want to update: ")
 		if update_contact == 'x':
@@ -69,7 +73,7 @@ def on_load():
 		for c in search_update:
 			if update_contact == c.fname:
 				new_update = Contact.get(Contact.fname == update_contact)
-				update_field = input("Which field do you want to update? fname/lname/email/phone: ")
+				update_field = input("Which field do you want to update? fname/lname/email: ")
 				if update_field == 'fname':
 					new_update.fname = input("New first name: ")
 					new_update.save()
@@ -77,10 +81,10 @@ def on_load():
 					new_update.lname = input("New last name: ")
 					new_update.save()
 				elif update_field == 'email':
-					new_update.email == input("New email: ")
+					new_update.email = input("New email: ")
 					new_update.save()
 				elif update_field == 'phone':
-					new_update.phone == int(input("New phone number: "))
+					new_update.phone = input("New phone number: ")
 					new_update.save()
 			else:
 				print("Contact does not exist.")
@@ -101,6 +105,9 @@ def on_load():
 				elif affirm == 'n':
 					print("Oh, ok. That was close.")
 					on_load()
+				else:
+					print("Deletion aborted.")
+				on_load()
 			else:
 				print("Contact does not exist.")
 			on_load()
